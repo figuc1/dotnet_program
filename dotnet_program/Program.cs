41bf40129be263ea37bebf236b4d8b2c4ba4f2dc
figuc1 <figuc@hvfcu.org>
2024-08-01 14:01:13 -0400

    Initial code and submodule upload

---
+++ dotnet_program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_program;
+using dotnet_library;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        dotnet_library.Class1.PrintString("Hello, World!");
+    }
+}
